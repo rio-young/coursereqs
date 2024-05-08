14d98dd2f71ce41d64149c6b5891fbdd6c16bca5
michaeltu01 <michaeltu705@gmail.com>
2024-05-08 13:53:26 -0400

    updated visualizer

--- visualizer.js
+++ visualizer.js
@@ -8,66 +8,62 @@ const numSems = instance.signature("Semester").tuples().length;
 const stage = new Stage();
 
 /**
- * Initialize the semester grid
- * e.g. [ Sem1 | Sem2 | Sem3 | ... | Sem8 ]
+ * Initialize course schedule grid.
  */
-const semesterGrid = new Grid({
+
+const scheduleGrid = new Grid({
   grid_location: { x: 20, y: 20 },
-  cell_size: { x_size: 200, y_size: 100 },
-  grid_dimensions: { x_size: numSems, y_size: 1 },
+  cell_size: { x_size: 200, y_size: 50 },
+  grid_dimensions: { x_size: numSems - 1, y_size: 6 },
 });
 
 // Add the name of each semester into the grid
 for (let s = 0; s < numSems; s++) {
-  if (Semester.atom("Semester" + s) != null) {
+  if (Semester.atom("Semester" + (s + 1)) != null) {
     const semesterTbox = new TextBox({
-      text: Semester.atom("Semester" + s).id(),
+      text: Semester.atom("Semester" + (s + 1)).id(),
       coords: { x: 0, y: 0 },
       color: "black",
       fontsize: 16,
     });
-    semesterGrid.add({ x: s, y: 0 }, semesterTbox);
+    scheduleGrid.add({ x: s, y: 0 }, semesterTbox);
+  }
+}
+
+// Add the courses for each semester into the grid
+for (let s = 0; s < numSems; s++) {
+  if (Semester.atom("Semester" + (s + 1)) != null) {
+    const courseList = listCourses(Semester.atom("Semester" + (s + 1)));
+    if (courseList.length > 5) {
+      const errorTbox = new TextBox({
+        text: `Taking ${courseList.length} courses`,
+        coords: { x: 0, y: 0 },
+        color: "red",
+        fontsize: 12,
+      });
+      scheduleGrid.add({ x: s, y: 1 }, errorTbox);
+    } else {
+      for (let r = 1; r < 6; r++) {
+        const courseTbox = new TextBox({
+          text: courseList[r],
+          coords: { x: 0, y: 0 },
+          color: "black",
+          fontsize: 12,
+        });
+        scheduleGrid.add({ x: s, y: r }, courseTbox);
+      }
+    }
   }
 }
 
 function listCourses(semesterAtom) {
-  let ret = "";
   const courses = semesterAtom
     .join(courses_taken)
     .tuples()
     .map((tuple) => {
       return tuple.atoms();
     });
-  for (const courseAtom of courses) {
-    ret += courseAtom.toString() + ", ";
-  }
-  return ret;
+  return courses;
 }
-
-/**
- * Initialize the courses grid. Positioned directly underneath the semesterGrid.
- * e.g. [ Courses for Semester 1 | Courses for Semester 2 | ... | Courses for Semester 8 ]
- */
-const coursesGrid = new Grid({
-  grid_location: { x: 20, y: 40 },
-  cell_size: { x_size: 200, y_size: 400 },
-  grid_dimensions: { x_size: numSems, y_size: 1 },
-});
-
-// Add the courses for each semester into the grid
-for (let s = 0; s < numSems; s++) {
-  if (Semester.atom("Semester" + s) != null) {
-    const coursesTbox = new TextBox({
-      text: listCourses(Semester.atom("Semester" + s)),
-      coords: { x: 0, y: 0 },
-      color: "black",
-      fontsize: 12,
-    });
-    coursesGrid.add({ x: s, y: 0 }, coursesTbox);
-  }
-}
-
-// Add the table to `stage` and render it
-stage.add(semesterGrid);
-stage.add(coursesGrid);
+stage.add(scheduleGrid);
 stage.render(svg, document);
