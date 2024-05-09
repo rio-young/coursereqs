e9f56254a51c76c7689f021ec13cceb082f8f83e
michaeltu01 <michaeltu705@gmail.com>
2024-05-09 12:02:27 -0400

    visualizer bug fixes

--- visualizer.js
+++ visualizer.js
@@ -4,6 +4,9 @@
 
 // Compute the number of semesters
 const numSems = instance.signature("Semester").tuples().length;
+const semesterAtoms = Semester.atoms();
+
+const errorMessages = [];
 
 const stage = new Stage();
 
@@ -19,9 +22,9 @@ const scheduleGrid = new Grid({
 
 // Add the name of each semester into the grid
 for (let s = 0; s < numSems; s++) {
-  if (Semester.atom("Semester" + s) != null) {
+  if (semesterAtoms[s] != null) {
     const semesterTbox = new TextBox({
-      text: Semester.atom("Semester" + s).id(),
+      text: semesterAtoms[s].id(),
       coords: { x: 0, y: 0 },
       color: "black",
       fontsize: 16,
@@ -32,16 +35,15 @@ for (let s = 0; s < numSems; s++) {
 
 // Add the courses for each semester into the grid
 for (let s = 0; s < numSems; s++) {
-  if (Semester.atom("Semester" + s) != null) {
-    const courseList = listCourses(Semester.atom("Semester" + s));
+  if (semesterAtoms[s] != null) {
+    const sem = semesterAtoms[s];
+    const courseList = listCourses(sem);
     if (courseList.length > 5) {
-      const errorTbox = new TextBox({
-        text: `Taking ${courseList.length} courses`,
-        coords: { x: 0, y: 0 },
-        color: "red",
-        fontsize: 12,
-      });
-      scheduleGrid.add({ x: s, y: 1 }, errorTbox);
+      errorMessages.push(
+        `Taking ${
+          courseList.length
+        } courses in ${sem.id()}. Try increasing your Int bitwidth to 6.`
+      );
     } else {
       for (let r = 1; r < 6; r++) {
         const courseTbox = new TextBox({
@@ -65,5 +67,18 @@ function listCourses(semesterAtom) {
     });
   return courses;
 }
+
 stage.add(scheduleGrid);
+
+for (let i = 0; i < errorMessages.length; i++) {
+  const error = errorMessages[i];
+  const errorTbox = new TextBox({
+    text: error,
+    coords: { x: 500, y: 400 + i * 50 },
+    color: "red",
+    fontsize: 12,
+  });
+  stage.add(errorTbox);
+}
+
 stage.render(svg, document);
